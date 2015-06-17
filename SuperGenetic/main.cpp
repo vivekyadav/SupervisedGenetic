1d3bc05bc937b87791bbdef003258346cd7e0e8b
Vivek Yadav <yadvivek@amazon.com>
2015-06-17 19:22:08 +0530

    Initial Commit

---
+++ SuperGenetic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SuperGenetic
+//
+//  Created by D, Vivek on 17/06/15.
+//  Copyright (c) 2015 Zero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
